db15695c1d0830cdf7b257e213e91210df149d3c
Richard Aom <com@richardatom.com>
2021-03-06 22:21:05 +0100

    0.0.6

--- base.go
+++ base.go
@@ -1,35 +1,87 @@
 package jet
 
 import (
-  "os"
-  "net/http"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path/filepath"
 )
+
+const maxUploadSize = 600 * 1024 * 1024 // 60 mb
+
+func renderError(w http.ResponseWriter, message string, statusCode int) {
+	w.WriteHeader(http.StatusBadRequest)
+	w.Write([]byte(message))
+}
 
 //FileExists is check if the file exists
 func FileExists(filepath string) bool {
-    info, err := os.Stat(filepath)
-    if os.IsNotExist(err) {
-        return false
-    }
-    return !info.IsDir()
+	info, err := os.Stat(filepath)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
 }
 
 //EnsureDir is check and make folder
 func EnsureDir(dirName string) error {
 
-    err := os.Mkdir(dirName, 0755)
+	err := os.Mkdir(dirName, 0755)
 
-    if err == nil || os.IsExist(err) {
-      return nil
-    }
-		return err
+	if err == nil || os.IsExist(err) {
+		return nil
+	}
+	return err
 }
+
 //ParamCheck sends back the URL parameters value
 func ParamCheck(param string, r *http.Request, fallback string) (string, bool) {
-  keys, ok := r.URL.Query()[param]
+	keys, ok := r.URL.Query()[param]
 
-      if !ok || len(keys[0]) < 1 {
-          return fallback, false
-      }
-      return keys[0], true
+	if !ok || len(keys[0]) < 1 {
+		return fallback, false
+	}
+	return keys[0], true
+}
+
+//UpFile is a helper for upload files
+func UpFile(uploadPath string) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// validate file size
+		r.Body = http.MaxBytesReader(w, r.Body, maxUploadSize)
+		if err := r.ParseMultipartForm(maxUploadSize); err != nil {
+			renderError(w, "FILE_TOO_BIG", http.StatusBadRequest)
+			return
+		}
+
+		// parse and validate file and post parameters
+		fileName := r.PostFormValue("name")
+		file, _, err := r.FormFile("uploadFile")
+		if err != nil {
+			renderError(w, "INVALID_FILE", http.StatusBadRequest)
+			return
+		}
+		defer file.Close()
+		fileBytes, err := ioutil.ReadAll(file)
+		if err != nil {
+			renderError(w, "INVALID_FILE", http.StatusBadRequest)
+			return
+		}
+
+		newPath := filepath.Join(uploadPath, fileName)
+		//fmt.Printf("File: %s\n", newPath)
+
+		// write file
+		newFile, err := os.Create(newPath)
+		if err != nil {
+			renderError(w, "CANT_WRITE_FILE", http.StatusInternalServerError)
+			return
+		}
+		defer newFile.Close() // idempotent, okay to call twice
+		if _, err := newFile.Write(fileBytes); err != nil || newFile.Close() != nil {
+			renderError(w, "CANT_WRITE_FILE", http.StatusInternalServerError)
+			return
+		}
+		w.Write([]byte(newPath))
+	})
 }
--- json.go
+++ json.go
@@ -1,45 +1,43 @@
 package jet
 
 import (
-  "fmt"
-  "io/ioutil"
+	"fmt"
+	"io/ioutil"
 )
 
 //DoDaJSON is checking if a json file is existing and if makeit is true creating it
-func DoDaJSON(makeit bool, filename string, path string, basepath string, basebyte []byte) (string) {
+func DoDaJSON(makeit bool, filename string, path string, basepath string, basebyte []byte) string {
 
-  filepathy := fmt.Sprintf("%s%s", path, filename)
+	filepathy := fmt.Sprintf("%s%s", path, filename)
 
-  if(makeit) {
-    //make the folder
-    if err := EnsureDir(path); err != nil {
-      fmt.Println("Directory creation failed with error: " + err.Error())
-    }
+	if makeit {
+		//make the folder
+		if err := EnsureDir(path); err != nil {
+			fmt.Println("Directory creation failed with error: " + err.Error())
+		}
 
-  	if FileExists(filepathy) == false {
-      var input []byte
-      var err error
+		var input []byte
+		var err error
 
-      if(basepath == ""){
-        input = basebyte
-      }else{
-        input, err = ioutil.ReadFile(basepath)
-        if err != nil {
-          fmt.Println("ReadFile on basepath" + err.Error())
-        }
-      }
+		if basepath == "" {
+			input = basebyte
+		} else {
+			input, err = ioutil.ReadFile(basepath)
+			if err != nil {
+				fmt.Println("ReadFile on basepath" + err.Error())
+			}
+		}
 
-  		err = ioutil.WriteFile(filepathy, input, 0644)
-  		if err != nil {
-  			fmt.Println("WriteFile on input" + err.Error())
-  		}
-  	}
-  }
-  //megnyitni a fájlt
-  file, err := ioutil.ReadFile(filepathy)
-  if err != nil {
-    fmt.Println("ReadFile on filepathy" + err.Error())
-  }
-  filestr := string(file)
-  return filestr
+		err = ioutil.WriteFile(filepathy, input, 0644)
+		if err != nil {
+			fmt.Println("WriteFile on input" + err.Error())
+		}
+	}
+	//megnyitni a fájlt
+	file, err := ioutil.ReadFile(filepathy)
+	if err != nil {
+		fmt.Println("ReadFile on filepathy" + err.Error())
+	}
+	filestr := string(file)
+	return filestr
 }
